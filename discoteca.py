6d82abad75b32bdb479fe754a0aa0dac3bde40b4
Sergio H. Valenzuela C <serginho61@gmail.com>
2022-10-26 21:13:21 -0400

    discoteca versión 0.3

--- discoteca.py
+++ discoteca.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('lista.html')
+    return render_template('lista.html', titulo='canciones')
 
 app.run(host="0.0.0.0", port=5000)
